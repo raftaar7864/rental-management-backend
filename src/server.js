399f658c270d088d42318e12a5532cd67f99832c
raftaar7864 <140149991+raftaar7864@users.noreply.github.com>
2025-11-21 17:14:42 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -38,14 +38,6 @@ app.use(express.json({
 
 app.use(morgan('dev'));
 
-app.use((req, res, next) => {
-  res.setHeader("X-Content-Type-Options", "nosniff");
-  res.setHeader("X-Frame-Options", "SAMEORIGIN");
-  res.setHeader("X-XSS-Protection", "1; mode=block");
-  next();
-});
-
-
 // -------------------- Routes --------------------
 const authRoutes = require('./routes/authRoutes');
 const buildingRoutes = require('./routes/buildingRoutes');
